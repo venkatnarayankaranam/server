bc5a61aecfa55fc8442c8a380cba94020fb5380c
venkatnarayankaranam <venkatnarayanchowdary95@gmail.com>
2025-04-06 09:24:15 +0530

    Added Origin

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ connectWithRetry();
 
 // Update CORS configuration
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'https://outing-alpha.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
